ab10f3ef794369c5b70aa268f8334018cb00037b
Service Web <sw@gssmail.com>
2024-03-12 09:01:28 -0500

    Commit changes in C:\Websites\Git\emarlow\4483\gab_4483_batch_notes.sql

---
+++ gab_4483_batch_notes.sql
@@ -0,0 +1,8 @@
+/* ==========================================
+ * TABLE: GAB_4483_BATCH_NOTES
+ * ========================================== */
+CREATE TABLE "GAB_4483_BATCH_NOTES"(
+ "ORDER_NO" CHAR(7) NOT NULL ,
+ "NOTES" CHAR(250));
+
+
