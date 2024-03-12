80b4e1cb47daa6ae29c70cf2385a04dbd846cb31
Service Web <sw@gssmail.com>
2024-03-12 09:01:28 -0500

    Commit changes in C:\Websites\Git\emarlow\4483\gab_4483_oe_ln_macro.sql

---
+++ gab_4483_oe_ln_macro.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GAB_4483_OE_LN_MACRO
+ * ========================================== */
+CREATE TABLE "GAB_4483_OE_LN_MACRO"(
+ "ORDER_NO" CHAR(7) NOT NULL ,
+ "RECORD_NO" CHAR(3) NOT NULL ,
+ "NAME" CHAR(30) NOT NULL ,
+ "VALUE" CHAR(30));
+
+
